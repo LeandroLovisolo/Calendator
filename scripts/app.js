add4586f867628e6f9b345ee83d0c52f089d0e73
Leandro Lovisolo <leandro@leandro.me>
2011-08-23 07:46:55 -0300

    Added code for adding and removing weeks

---
+++ scripts/app.js
@@ -0,0 +1,51 @@
+var startDate, currentDate = null;
+
+$(function() {
+	startDate = Date.today();
+	while(!startDate.is().sunday()) startDate.add(-1).days();
+	currentDate = startDate;
+	
+	// Populate the calendar
+	$("tbody").empty();
+	for(var i = 0; i < 4; i++) addWeek();
+	
+	// Bind events
+	$("#more").click(addWeek);
+	$("#less").click(removeWeek);
+});
+
+function addWeek() {
+	var row = $("<tr></tr>");
+	row.append("<td class='month'></td>");
+	var newMonth = false;
+	
+	for(var i = 0; i < 7; i++) {
+		row.append("<td>" + currentDate.getDate() + "</td>");
+		if(currentDate.getDate() == 1) newMonth = true;
+		currentDate.add(1).days();
+	}
+	
+	row.append("<td class='comments'><input type='text'/></td>");
+	
+	if(newMonth || $("tbody").children().length == 0) {
+		row.find("td.month").text(currentDate.toString("MMM yyyy"));
+	}
+	
+	$("tbody").append(row);
+	updateLessLink();
+}
+
+function removeWeek() {
+	if($("tbody tr").length == 1) return;
+	$("tbody tr:last").remove();
+	currentDate.add(-7).days();
+	updateLessLink();
+}
+
+function updateLessLink() {
+	if($("tbody tr").length == 1) {
+		$("#less").addClass("disabled");
+	} else {
+		$("#less").removeClass("disabled");	
+	}
+}
