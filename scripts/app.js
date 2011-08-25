18f44af72593beae24c73b72a0fe947f9f0ed7ab
Leandro Lovisolo <leandro@leandro.me>
2011-08-25 00:35:39 -0300

    The app now remembers marked days and comments when re-adding previously removed weeks

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,6 @@
 var startDate, currentDate = null;
 var startMouseY = 0;
+var markedDates = [], comments = [];
 
 $(function() {
 	startDate = Date.today();
@@ -42,17 +43,68 @@ function addWeek() {
 	var row = $("<tr></tr>");
 	row.append("<td class='month'></td>");
 	var newMonth = false;
-	
+
+	// Prepare the comments cell
+	var commentsCell = $("<td class='comments'><input type='text' maxlength='40'/></td>");
+
+	// Remember comments
+	commentsCell.find("input")[0].date = currentDate.clone();
+	var rememberComments = function() {
+		var comment = null;
+		for(var i = 0; i < comments.length; i++) {
+			if(comments[i].date.compareTo(this.date) == 0) {
+				comment = comments[i];
+			}
+		}
+		if(comment == null) {
+			comment = {date: this.date, text: null};
+			comments.push(comment);
+		}
+		comment.text = $(this).val();
+	};
+	commentsCell.find("input").keyup(rememberComments);
+	commentsCell.find("input").change(rememberComments);
+
+	// Load previously entered comments
+	for(var i = 0; i < comments.length; i++) {
+		if(comments[i].date.compareTo(currentDate) == 0) {
+			commentsCell.find("input").val(comments[i].text);
+		}
+	}	
+
 	for(var i = 1; i <= 7; i++) {
 		var cell = $("<td class='date'>" + currentDate.getDate() + "</td>");
-		if(i == 1 || i == 7) cell.addClass("weekend");
-		cell.click(toggleDate);
+		if(!currentDate.isWeekday()) cell.addClass("weekend");
+
+		// Add the "marked" class if the user marked this date
+		for(var j = 0; j < markedDates.length; j++) {
+			if(markedDates[j].compareTo(currentDate) == 0) {
+				cell.addClass("marked");
+				break;
+			}
+		}
+
+		// Mark/unmark dates when clicked
+		cell[0].date = currentDate.clone();
+		cell.click(function() {
+			var wasMarked = false;
+			for(var j = 0; j < markedDates.length; j++) {
+				if(markedDates[j].compareTo(this.date) == 0) {
+					markedDates.splice(j, 1);
+					wasMarked = true;
+					break;
+				}
+			}
+			if(!wasMarked) markedDates.push(this.date);
+			$(this).toggleClass("marked");			
+		});
+
 		row.append(cell);
 		if(currentDate.getDate() == 1) newMonth = true;
 		currentDate.add(1).days();
 	}
 	
-	row.append("<td class='comments'><input type='text' maxlength='40'/></td>");
+	row.append(commentsCell);
 	
 	if(newMonth || $("tbody").children().length == 0) {
 		row.find("td.month").text(currentDate.toString("MMM yyyy"));
@@ -66,8 +118,3 @@ function removeWeek() {
 	$("tbody tr:last").remove();
 	currentDate.add(-7).days();
 }
-
-function toggleDate() {
-	$(this).toggleClass("marked");
-}
-
